9203da4c78326b9d9a0ec991e85c04b1aec0dd17
annazineldin3 <anna.zineldin@gmail.com>
2026-08-18 15:33:01 +0200

    completed

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -2,6 +2,9 @@ package com.booleanuk.core;
 
 import com.booleanuk.helpers.ExerciseBase;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercise extends ExerciseBase {
     /*
         A method is a function, a single piece of logic that can run. In Java, a class is a convenient
@@ -35,7 +38,7 @@ public class Exercise extends ExerciseBase {
         Complete this method so that it increases the number given by 1 and returns the result
      */
     public int increment(int number) {
-        return 0;
+        return number + 1;
     }
 
     /*
@@ -48,8 +51,8 @@ public class Exercise extends ExerciseBase {
         Nathan | Hi, Nathan :)
         Edward | Hi, Edward :)
      */
-    public String happilyGreet() {
-        return "Not implemented yet";
+    public String happilyGreet(String name) {
+        return "Hi, " + name + " :)";
     }
 
     /*
@@ -64,6 +67,13 @@ public class Exercise extends ExerciseBase {
         10, 13 | [10,11,12,13]
         -1, 1  | [-1,0,1]
      */
+public int[] constructNumberArray(int lower, int upper){
+int[] numbers = new int[upper - lower + 1];
+for(int i = 0; i < numbers.length; i++) {
+    numbers[i] = lower + i;
+}
+    return numbers;
+}
 
 
 
@@ -80,6 +90,15 @@ public class Exercise extends ExerciseBase {
         disaster, 5 | DISASTER!!!!!
         error, 10   | ERROR!!!!!!!!!!
      */
+    public String shout(String str, int number) {
+        String result = str.toUpperCase();
+
+        for (int i = 0; i < number; i++) {
+            result = result + "!";
+        }
+
+        return result;
+    }
 
 
 
